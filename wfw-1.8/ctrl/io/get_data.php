75481f576bcd2a6b86eaa209bb9ce592d1195d49
Thomas AUGUEY <contact@aceteam.org>
2013-04-08 14:47:58 +0200

    fix getData controler

--- wfw-1.8/ctrl/io/get_data.php
+++ wfw-1.8/ctrl/io/get_data.php
@@ -1,7 +1,7 @@
 <?php
 /*
     ---------------------------------------------------------------------------------------------------------------------------------------
-    (C)2012-2013 Thomas AUGUEY <contact@aceteam.org>
+    (C)2013 Thomas AUGUEY <contact@aceteam.org>
     ---------------------------------------------------------------------------------------------------------------------------------------
     This file is part of WebFrameWork.
 
@@ -21,47 +21,33 @@
 */
 
 /*
- * Détermine le statut d'un envoie
+ * Affiche les données d'un fichier uploadé dans la sortie
  * Rôle : Administrateur
- * UC   : check_upload
+ * UC   : get_data
  */
 
 class Ctrl extends cApplicationCtrl{
     public $fields    = array('io_upload_id');
-    public $op_fields = null;
+    public $op_fields = array('encoded_output');
 
     function main(iApplication $app, $app_path, $p)
     {
         if(!IoUploadMgr::getById($upload, $p->io_upload_id))
             return false;
         
-        //termine
-        //if($upload->uploadComplete == "TRUE")
-        //    return RESULT(cResult::Ok,"IO_FILE_COMPLETED");
+        $result = cResult::getLast();
         
-        $result = new cResult(cResult::Failed,"IO_FILE_UNCOMPLETED");
-        /*$result->addAtt("status","UNCOMPLETED");
-        $result->addAtt("packet_count",$upload->packetCount);
-        $result->addAtt("packet_size",$upload->packetSize);
-        $result->addAtt("file_size",$upload->fileSize);
-        $result->addAtt("last_packet_size",$upload->fileSize-(($upload->packetCount-1)*$upload->packetSize));
-        $result->addAtt("last_packet_num",$upload->packetCount-1);*/
-        for($i=0; $i<$upload->packetCount; $i++)
-        {
-            $query = "select count(*) as cnt from io_packet where io_upload_id='$p->io_upload_id' and packet_num=$i and packet_status=TRUE;";
-            if(!$app->queryToObject($query,$resp))
-                return false;
-            if($resp->cnt == "0"){
-                $result->addAtt("packet_num",$i);
-                $result->addAtt("packet_offset",$upload->packetSize*$i);
-                if($i == $upload->packetCount-1)//dernier paquet ? alors la taille n'est pas fixe
-                    $result->addAtt("packet_size",$upload->fileSize-(($upload->packetCount-1)*$upload->packetSize));
-                else
-                    $result->addAtt("packet_size",$upload->packetSize);
-                return RESULT_INST($result);
-            }
-        }
-        return RESULT(cResult::Ok,"IO_FILE_COMPLETED");
+        //decode les paquets
+        $data=NULL;
+        if(!IOModule::getData($upload,$data))
+            return false;
+        
+        //sortie
+        header("Content-Type:".$upload->contentType);
+        header('Content-Description: '.$upload->filename);
+        header('Content-Disposition: inline; filename="'.$upload->filename.'"');
+        echo($data);
+        exit;
     }
 };
 
